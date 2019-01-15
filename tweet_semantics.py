fc79f25bab655f9c980c9f311fa32688848b2a42
pdellagrotte <paul.dellagrotte@gmail.com>
2019-01-14 20:24:50 -0500

    Stable version

--- tweet_semantics.py
+++ tweet_semantics.py
@@ -1,22 +1,26 @@
 # main module
 from textblob import TextBlob
+from utilities import database_utilities
 from get_tweets import got_tweet_methods
 
-
+# Loop through Twitter handles in text file, pass to Twitter API, and return values for storage in db
 def main():
+    database_utilities.create_database()  # Creates the database
+
     # Open the file with twitter handles to query tweets
     twitter_handles_file = open('handles.txt', 'r')
     lines = twitter_handles_file.readlines()
 
+    # Saves Tweets to database
     for handle in lines:
         tweets = got_tweet_methods.return_tweets(handle)
         # print(tweets)
         for tweet in tweets:
-            print(handle, tweet.full_text)
-
-
-    # Save tweets to database
-    # TODO: Write code to save tweets to database
+            b = TextBlob(tweet.full_text)
+            tweet_list = [handle, tweet.full_text, tweet.created_at, b.sentiment[0]]
+            database_utilities.save_tweets_to_database(tweet_list)
+        print(handle + " records saved to database")
+    print("End of program")
 
 if __name__ == "__main__":
     main()
--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'words.db')
+DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'tweets.db')
 
 
 # Create database to store tweets for different Twitter Handles and Semantics
@@ -9,28 +9,22 @@ def create_database(database_path=DEFAULT_PATH):
     conn = sqlite3.connect(database_path)
     with conn:
         cur = conn.cursor()
-        cur.execute("drop table if exists words")
-        ddl = """CREATE TABLE tweets (handle TEXT PRIMARY KEY NOT NULL, 
-        tweet_text TEXT, polarity INT NOT NULL )"""
+        cur.execute("drop table if exists tweets")
+        ddl = """CREATE TABLE tweets (handle, 
+        tweet_text TEXT, created_at TEXT, polarity INT NOT NULL )"""
         cur.execute(ddl)
     conn.close()
+
 
 # Saves tweets to the database and
 def save_tweets_to_database(tweet_list: list, database_path=DEFAULT_PATH):
     conn = sqlite3.connect(database_path)
     with conn:
         cur = conn.cursor()
-        for tweet in tweet_list:
-            # check to see if the words is in there
-            sql = "select count(word) from words where word='" + tweet + "'"
-            cur.execute(sql)
-            count = cur.fetchone()[0]
-            if count > 0:
-                sql = "update words set usage_count = usage_count + 1 where word = '" + word + "'"
-            else:
-                sql = "insert into words(word) values ('" + word + "')"
-            cur.execute(sql)
-        print("Database save complete!")
+        sql = "INSERT INTO tweets(handle, tweet_text, created_at, polarity) " \
+              "VALUES(?, ?, ?, ?)"
+        cur.execute(sql, tweet_list)
+
 
 # Execute a SQL statement against created database
 def execute_query(sql: str, database_path=DEFAULT_PATH):
